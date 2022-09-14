ac3c4ca1aa613246cb0821d9b1f743db2f274b6f
Pablo Zumarán <pablozumaran@inacapmail.cl>
2022-09-14 15:49:34 -0300

    actualizacion 2.0

--- Clase 12 Septiembre/veterinaria/gatos/views.py
+++ Clase 12 Septiembre/veterinaria/gatos/views.py
@@ -1,4 +1,5 @@
 from cgitb import html
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -6,12 +7,22 @@ from django.http import HttpResponse
 
 
 def saludo (request):
-    return render(request,"./gatos/templates/menu.html")
+    context ={
+        "mensaje":"bienvenidos"
+    }
+
+    return render(request,"./gatos/templates/menu.html",context)
 
 
 def agregar (request):
     return render(request,"./gatos/templates/index.html")
 
 def lista (request):
-    gato = {"gato1":[{"nombre":"juanito"},{"edad":"2años"},{"raza":"Abisinio"},{"genero":"macho"},{"color":"cafe"},{"peso":"18kg"}]}
-    return render(request,"./gatos/templates/lista.html",gato)
+    context = {
+        "gatos" : [{"nombre":"juan","edad":"3 años","raza":"albino","genero":"macho","color":"negro","peso":"12kg"},
+    {"nombre":"femina","edad":"1 año","raza":"Callejero","genero":"Hembra","color":"Blanca","peso":"2kg"},
+    {"nombre":"Maria","edad":"5 años","raza":"nulo","genero":"Hembra","color":"blanca","peso":"4kg"},
+    {"nombre":"pedrito","edad":"6","raza":"n","genero":"m","color":"gris","peso":"8kg"}]
+    }
+    
+    return render(request,"./gatos/templates/lista.html",context)
