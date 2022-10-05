b6b5db152283fbb3063a76e5bf01e04dec3d3f93
pablozumaran18 <pablo.zumaran@inacapmail.cl>
2022-10-05 15:57:33 -0500

    actualizacion 05 de octubre

---
+++ Clase 05 de octubre/movies/cartelera/models.py
@@ -0,0 +1,9 @@
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+# Create your models here.
+
+
+class movie (models.Model) :
+    name = models.CharField(max_length = 100)
+    comentario = models.TextField()
